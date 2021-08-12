e0230b6fffe4c9d8a6fe03760fb58177d3611f0b
som apiwatcharoenkul <o00o_4@hotmail.com>
2021-08-12 13:19:01 +0700

    - change class type of FindItagPage form StatefulWidget to StatelessWidget - delete unused comment

--- lib/main.dart
+++ lib/main.dart
@@ -42,72 +42,60 @@ class BtOffPage extends StatelessWidget {
   }
 }
 
-class FindItagPage extends StatefulWidget {
+class FindItagPage extends StatelessWidget {
   const FindItagPage({Key? key}) : super(key: key);
 
   @override
-  _FindItagPageState createState() => _FindItagPageState();
-}
-
-class _FindItagPageState extends State<FindItagPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Searching iTag ...'),
-      ),
-      body: Center(
-          child: SingleChildScrollView(
-        child: Column(
-          children: [
-            StreamBuilder<List<ScanResult>>(
-              stream: FlutterBlue.instance.scanResults,
-              initialData: [],
-              builder: (context, snapshot) {
-                return Column(
-                    children: snapshot.data!
-                        .map(
-                            // เอาข้อมูล scanResult มาทำเป็น List ของ Widget Text แสดงค่า bt device id + name ที่scan เจอ
-                            (r) => Text('id: ${r.device.id},  name: ${r.device.name}'))
-                        .toList());
-              },
-            )
-          ],
+        appBar: AppBar(
+          title: Text('Searching iTag ...'),
         ),
-      )),
-      floatingActionButton: StreamBuilder<bool>(
-        stream: FlutterBlue.instance.isScanning, // stream ค่าสถานะการ scan ble ว่ากำลัว scan อยู่รึเปล่า
-        initialData: false,
-        builder: (context, snapshot) {
-          if (snapshot.data!) {
-            // bt is scanning
-            return FloatingActionButton(
-              child: Icon(Icons.stop),
-              onPressed: () {
-                // ถ้าระหว่างที่ bt กำลัง scan เรากด stop การ scan ได้
-                FlutterBlue.instance.stopScan();
-              },
-              backgroundColor: Colors.red,
-            );
-          } else {
-            // bt is NOT scanning
-            return FloatingActionButton(
-              child: Icon(Icons.search),
-              onPressed: () {
-                // ถ้าไม่ใส่ timeout ในการ scan มันจะ scan ไม่หยุดเลย, ไม่อย่างนั้นต้องสั่ง .stopScan
-                FlutterBlue.instance.startScan(timeout: Duration(seconds: 10));
-              },
-            );
-          }
-        },
-      )
-    );
+        body: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  StreamBuilder<List<ScanResult>>(
+                    stream: FlutterBlue.instance.scanResults,
+                    initialData: [],
+                    builder: (context, snapshot) {
+                      return Column(
+                          children: snapshot.data!
+                              .map(
+                            // เอาข้อมูล scanResult มาทำเป็น List ของ Widget Text แสดงค่า bt device id + name ที่scan เจอ
+                                  (r) => Text('id: ${r.device.id},  name: ${r.device.name}'))
+                              .toList());
+                    },
+                  )
+                ],
+              ),
+            )),
+        floatingActionButton: StreamBuilder<bool>(
+          stream: FlutterBlue.instance.isScanning, // stream ค่าสถานะการ scan ble ว่ากำลัว scan อยู่รึเปล่า
+          initialData: false,
+          builder: (context, snapshot) {
+            if (snapshot.data!) {
+              // bt is scanning
+              return FloatingActionButton(
+                child: Icon(Icons.stop),
+                onPressed: () {
+                  // ถ้าระหว่างที่ bt กำลัง scan เรากด stop การ scan ได้
+                  FlutterBlue.instance.stopScan();
+                },
+                backgroundColor: Colors.red,
+              );
+            } else {
+              // bt is NOT scanning
+              return FloatingActionButton(
+                child: Icon(Icons.search),
+                onPressed: () {
+                  // ถ้าไม่ใส่ timeout ในการ scan มันจะ scan ไม่หยุดเลย, ไม่อย่างนั้นต้องสั่ง .stopScan
+                  FlutterBlue.instance.startScan(timeout: Duration(seconds: 10));
+                },
+              );
+            }
+          },
+        )
+    );;
   }
 }
-
-// FloatingActionButton(
-// child: Icon(Icons.search),
-// onPressed: () {
-// FlutterBlue.instance.startScan(timeout: Duration(seconds: 4)); // ถ้าไม่ใส่ timeout ในการ scan มันจะ scan ไม่หยุดเลย, ไม่อย่างนั้นต้องสั่ง .stopScan
-// },
-// ),
